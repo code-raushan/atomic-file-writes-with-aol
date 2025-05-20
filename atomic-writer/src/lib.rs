8e7391dc2644a4adb2dac46e5803e4aee5d1556a
Raushan Kumar <raushan.development@gmail.com>
2025-05-20 19:37:54 +0530

    feat: atomic write function

--- atomic-writer/src/lib.rs
+++ atomic-writer/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{fs::{self, File}, io::{self, Write}, path::{Path, PathBuf}};
+use uuid::Uuid;
+
+
+pub fn atomic_write(path: &Path, data: &[u8]) -> io::Result<()> {
+    let temp_path = generate_temp_path(path)?;
+
+    let mut temp_file = File::create(&temp_path)?;
+
+    temp_file.write_all(data)?;
+    // fsync 
+    temp_file.sync_all()?;
+
+    // Atomic replace -> by default, the file will be replaced atomically
+    fs::rename(temp_path, path)?;
+
+    Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn generate_temp_path(target: &Path) -> io::Result<PathBuf> {
+    let dir = target.parent().ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Target Path is invalid"))?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    Ok(dir.join(format!(".tmp.{}", Uuid::new_v4())))
 }
+
