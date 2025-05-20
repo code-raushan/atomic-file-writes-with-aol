391c9768569a08ef14616365b4360d4a49388ec4
Raushan Kumar <raushan.development@gmail.com>
2025-05-20 21:00:20 +0530

    feat: main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{fs, path::Path};
+use atomic_writer::atomic_write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("--- Atomic Write Test ---");
+    let atomic_path = Path::new("atomic_data.txt");
+    let atomic_data = b"Testing atomic writes";
+
+    match atomic_write(atomic_path, atomic_data) {
+        Ok(_) => {
+            println!("Atomic write successfully completed");
+
+            match fs::read(atomic_path) {
+                Ok(data) => {
+                    assert_eq!(data, atomic_data, "Written data does not match the original data");
+                }
+                Err(e) => {
+                    eprintln!("Failed to read atomic file: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Atomic write failed: {}", e);
+        }
+    }
+
+    println!("--- Append-Only Log Test ---");
+    
 }
